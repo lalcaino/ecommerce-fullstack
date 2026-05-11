46d61119dfd80d427317bed5559667887bb30ff7
escriabin <escriabin13@gmail.com>
2026-05-11 09:16:13 -0400

    Modificación Slider en Services

--- frontend/src/components/Sections/Services.jsx
+++ frontend/src/components/Sections/Services.jsx
@@ -23,9 +23,6 @@ export default function Services() {
   return (
     <Wrapper id="services">
       <div className="lightBg" style={{ padding: "50px 0" }}>
-        <div className="container">
-          <ClientSlider />
-        </div>
       </div>
 
       <div className="whiteBg" style={{ padding: "60px 0" }}>
